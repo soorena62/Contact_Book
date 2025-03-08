678a81adcf5f1583abfb3c3d375435c3a984bfd1
soorena62 <majidneg738@gmail.com>
2025-03-08 16:36:03 +0330

    Create Views Loginview, ContactDetailView And ContactListView

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,113 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import Contact, CustomUser
+from .serializers import ContactSerializer, CustomUserSerializer
+# Create your views here:
+
+
+class UserListCreateView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        users = CustomUser.objects.all()
+        serializer = CustomUserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetailView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get_user(self, pk):
+        try:
+            return CustomUser.objects.get(pk=pk)
+        except CustomUser.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        user = self.get_user(pk)
+        if user:
+            serializer = CustomUserSerializer(user)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        user = self.get_user(pk)
+        if not user:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = CustomUserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = self.get_user(pk)
+        if not user:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+class ContactListView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        contacts = Contact.objects.filter(user=request.user)
+        serializer = ContactSerializer(contacts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ContactDetailView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_contact(self, request, pk):
+        try:
+            return Contact.objects.get(pk=pk, user=request.user)
+        except Contact.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        contact = self.get_contact(request, pk)
+        if contact:
+            serializer = ContactSerializer(contact)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        contact = self.get_contact(request, pk)
+        if not contact:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ContactSerializer(contact, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        contact = self.get_contact(request, pk)
+        if not contact:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        contact.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class LoginView(TokenObtainPairView):
+    permission_classes = [permissions.AllowAny]
